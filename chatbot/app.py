b8f3e99fd255b3779669ac61708675b7bcaccad8
sotojang <mrjang28@gmail.com>
2023-06-09 11:27:07 +0900

    Add files via upload

---
+++ chatbot/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+
+openai.api_key = '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'
+
+app = Flask(__name__)
+
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    message = request.json['message']
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+        ]
+    )
+    return jsonify(response['choices'][0]['message']['content'])
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+def main():
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
+if __name__ == "__main__":
+    main()
